50a001214b683c5631c1da1f3119452b09daa5a4
finaclemence15 <finaclemence15@gmail.com>
2019-09-13 13:22:33 +0200

    adding classes

---
+++ app/models.py
@@ -0,0 +1,57 @@
+
+ 
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
+
+
+class Articles:
+    '''
+    Articles class to define Articles Objects
+    '''
+
+    def __init__(self,title,image,description,url,date):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.url = url
+        self.date = date
+        
+class Review:
+
+    all_reviews = []
+
+    def __init__(self,movie_id,title,imageurl,review):
+        self.news_id = news_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
+
+
+    def save_review(self):
+        Review.all_reviews.append(self)
+
+
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.news_id == id:
+                response.append(review)
+
+        return response        
---
+++ config.py
@@ -0,0 +1,21 @@
+ import os
+
+class Config:
+
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_SOURCES_BASE_URL='https://newapi.org/v2/sources?category&apiKey=5b632280af14422cac83cec2cc9314db'
+    NEWS_API_KEY = os.environ.get('5b632280af14422cac83cec2cc9314db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
