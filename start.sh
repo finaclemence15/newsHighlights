0843e0f33eab0bf68b95b54915ea9481a34cd71c
finaclemence15 <finaclemence15@gmail.com>
2019-09-13 17:56:49 +0200

    adding review file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=5b632280af14422cac83cec2cc9314db
-export SECRET_KEY=<Your secret key>
+export NEWS_API_KEY=5b632280af14422cac83cec2cc9314db
+# export SECRET_KEY=<Your secret key>
 
 python3.6 manage.py server
