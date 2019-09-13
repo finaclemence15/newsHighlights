ae628c6ae70b9d7c4667d3226137c1e6d42bcc99
finaclemence15 <finaclemence15@gmail.com>
2019-09-13 15:54:13 +0200

    creating a readme

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,10 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_source,get_articles
+from .forms import ReviewForm
+from ..models import Review
+
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
