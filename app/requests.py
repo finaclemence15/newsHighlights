4ac075d4eb198c32ab0b7e025ab58456e4e360a2
finaclemence15 <finaclemence15@gmail.com>
2019-09-14 23:05:28 +0200

    creating article class

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,4 @@
-import urllib.request,json
+import urllib.requests,json
 from .models import Source, Articles,Review
 
 # Getting api key
@@ -6,13 +6,17 @@ api_key = None
 # Getting the movie base url
 base_url = None
 
+#Getting articles base url
+articles_url = None
+
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url,articles_url
     api_key = app.config['NEWS_API_KEY'] 
     base_url = app.config['NEWS_API_BASE_URL']
+    articles_url = app.config['ARTICLES_BASE_URL']
     
     
-def get_sources(category):
+def get_source(category):
     '''
     Function that gets the json response to our url request.
     '''
